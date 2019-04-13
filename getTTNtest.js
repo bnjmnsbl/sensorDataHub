d0c428d1e0f3a35c9ca5b8683f9a1b23ff02440d
Benjamin <benjamin.seibel@gmx.de>
2019-04-14 00:30:50 +0200

    lint

--- getTTNtest.js
+++ getTTNtest.js
@@ -1,69 +1,70 @@
-let ttn = require("ttn"),
-config = require(__dirname + '/config.json'),
-SensorModel = require("./models/sensor"),
-db = require("./db");
+/* eslint-disable no-mixed-spaces-and-tabs */
+let ttn = require('ttn'),
+  config = require(__dirname + '/config.json'),
+  SensorModel = require('./models/sensor'),
+  db = require('./db');
 
 
 config.forEach((el)=> {
 
-	let region = el.region,
-	appId = el.appId,
-	accessKey = el.key;
-	owner = el.owner
+  let region = el.region,
+    appId = el.appId,
+    accessKey = el.key,
+    owner = el.owner;
 
-	ttn.data(appId, accessKey)
-	.then((client)=> {
-		console.log("connected: " + appId);
-		
-		client.on("uplink", (function(devId, message) {
-			SensorModel.findOne({"metadata.app_id": appId}, function(err, sensor) {
-				if (!sensor) {
-					console.log("Not found in DB. Creating new sensor entry")
-					
-					let data = {
-						"metadata": {
-							"app_id": message.app_id,
-							"hardware_serial": message.hardware_serial,
-							"frequency": message.metadata.frequency,
-							"owner": owner
-							
-							},
-						"payloads": {
-							"payload": message.payload_fields,
-							"timestamp": message.metadata.time 
-						}
-					}
-					let newSensor = new SensorModel(data);
+  ttn.data(appId, accessKey)
+    .then((client)=> {
+      console.log('connected: ' + appId);
 
-					newSensor.save(function(err, data) {
-						if (err) throw err
-						console.log("Sensor created: " + appId);
-					})
-				}
-				else {
+      client.on('uplink', (function(devId, message) {
+        SensorModel.findOne({'metadata.app_id': appId}, function(err, sensor) {
+          if (!sensor) {
+            console.log('Not found in DB. Creating new sensor entry');
 
-				let data = {
-					"payload": message.payload_fields,
-					"timestamp": message.metadata.time 
-				}
-				sensor.payloads.push(data);
-			    
-			    console.log("Sensor found: ");
+            let data = {
+              'metadata': {
+                'app_id': message.app_id,
+                'hardware_serial': message.hardware_serial,
+                'frequency': message.metadata.frequency,
+                'owner': owner
+
+              },
+              'payloads': {
+                'payload': message.payload_fields,
+                'timestamp': message.metadata.time
+              }
+            };
+            let newSensor = new SensorModel(data);
+
+            newSensor.save(function(err, data) {
+              if (err) throw err;
+              console.log('Sensor created: ' + appId);
+            });
+          }
+          else {
+
+            let data = {
+              'payload': message.payload_fields,
+              'timestamp': message.metadata.time
+            };
+            sensor.payloads.push(data);
+
+			    console.log('Sensor found: ');
 			    console.dir(data);
 
-				sensor.save(function(err, updatedData) {
-					if (err) throw err;
-					console.dir("I updated: " + appId);
-				})
-				}
-			})
-			
-		}));
-	})
+            sensor.save(function(err, updatedData) {
+              if (err) throw err;
+              console.dir('I updated: ' + appId);
+            });
+          }
+        });
+
+      }));
+    });
 
 
 
-})
+});
 
 
 
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+	jshint = require('jshint'),
+	uglify = require('uglify'),
+	pump = require('pump')
+
+
+gulp.task('compress', function(cb) {
+	var imgSrc = 
+})
